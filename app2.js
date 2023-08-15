a9f5b693d0c7fa04100bc3ef75d47936ce1fe522
Anikeet-dev <aniketkakde55@.com>
2023-08-15 15:35:54 +0530

    Added new list

--- app2.js
+++ app2.js
@@ -1,19 +1,41 @@
 
+//Get Element By Id. -->
+// let headerTitle = document.getElementById('main-header');
 
-let headerTitle = document.getElementById('main-header');
-let cardItems = document.getElementsByClassName('title');
+// headerTitle.style.borderBottom = 'solid 4px black';
 
-headerTitle.style.borderBottom = 'solid 4px black';
-for (let i = 0; i < cardItems.length; i++) {
-    if (cardItems[i].textContent === 'Add Items') {
-      cardItems[i].style.fontWeight = 'bold';
-      cardItems[i].style.color = 'green';
+// Get Element By ClassName. -->
+
+// let cardItems = document.getElementsByClassName('title');
+// console.log('cardItems');
+
+// for (let i = 0; i < cardItems.length; i++) {
+//     if (cardItems[i].textContent === 'Add Items') {
+//       cardItems[i].style.fontWeight = 'bold';
+//       cardItems[i].style.color = 'green';
+//     }
+//   }
+
+//   let items = document.getElementsByClassName('list-group-item');
+//   items[1].style.backgroundColor = 'green';
+
+//   for(let i = 0; i < items.length; i++){
+//     items[i].style.fontWeight = 'bold';
+//   }
+
+  //Get Element By TagName. -->
+  let li = document.getElementsByTagName('li');
+  console.group('li');
+  for (let i = 0; i < li.length; i++) {
+      if (li[i].textContent === 'Add Items') {
+        li[i].style.fontWeight = 'bold';
+        li[i].style.color = 'green';
+      }
     }
-  }
-
-  let items = document.getElementsByClassName('list-group-item');
-  items[1].style.backgroundColor = 'green';
-
-  for(let i = 0; i < items.length; i++){
-    items[i].style.fontWeight = 'bold';
-  }
+  
+    li[1].style.backgroundColor = 'green';
+  
+    for(let i = 0; i < li.length; i++){
+      li[i].style.fontWeight = 'bold';
+    }
+  
